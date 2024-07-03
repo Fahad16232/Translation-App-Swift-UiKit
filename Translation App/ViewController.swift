46494fa8a954631a68c13dc98b83aefd03605231
M.Jahanzaib Abid <150152743+Fahad16232@users.noreply.github.com>
2024-07-03 18:14:25 +0500

    Initial Commit

---
+++ Translation App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Translation App
+//
+//  Created by Fahad on 03/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
